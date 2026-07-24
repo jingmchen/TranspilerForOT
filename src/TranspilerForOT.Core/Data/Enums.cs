c79e2a6d5c4d165c54a0e8cc0e8d4647835dcedf
JM <jingming.tan12@gmail.com>
2026-07-24 06:17:07 -0700

    Add files via upload
    Added enums for VariableKind, VariableScopeKind, BinaryOperator, UnaryOperator

--- src/TranspilerForOT.Core/Data/Enums.cs
+++ src/TranspilerForOT.Core/Data/Enums.cs
@@ -5,3 +5,10 @@ namespace TranspilerForOT.Core.Data;
 // TranspilerForOT.Core.Abstractions.IThemeService
 public enum AppTheme {Light, Dark, System}
 public enum AppAccent {White, Black}
+
+public enum VariableScopeKind {External, Local}
+public enum VariableKind {Numeric, Boolean, Byte}
+public enum BinaryOperator {
+    Or, And, Equal, NotEqual, Less, LessOrEqual, Greater, GreaterOrEqual, Add, Subtract, Multiply, Divide
+}
+public enum UnaryOperator {Not, Negate}
